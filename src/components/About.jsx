0f4ec86846cadb71802706837997cb24002f971f
Astitva Singh <125200931+Astitvapg@users.noreply.github.com>
2023-04-16 14:18:05 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,11 +47,9 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        I'm a skilled  with experience in TypeScript and
+        Flutter with framework like Mern, Docker and Dart. I'Learned all this Skills From One of the Worlds Biggest
+                  Online Learning University Microsoft Imagine Academy and Jet-Brains Academy!
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
